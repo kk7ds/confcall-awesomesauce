8cd78a74d1fa8250674f879b87addd887e4bfc38
Dan Smith <dsmith@danplanet.com>
2013-06-26 16:18:11 -0700

    Add text/gui modes to gcal_notifier, and dryrun test helper

--- gcal_notifier.py
+++ gcal_notifier.py
@@ -6,6 +6,8 @@ import os
 import re
 import urllib
 import ConfigParser
+import optparse
+import sys
 
 import icalendar
 
@@ -46,29 +48,41 @@ def get_upcoming(cal, timerange):
             upcoming_calls[delta] = (item, code)
     return upcoming_calls
 
-def maybe_call_next(caldata):
-    cal = icalendar.Calendar.from_ical(caldata)
-    timerange = datetime.timedelta(minutes=int(
-            CONF.get('prefs', 'lookahead_minutes')))
-    calls = get_upcoming(cal, timerange)
+def gui_prompt_to_call_next(opts, delta, item, code):
     sound = CONF.get('prefs', 'alert_sound')
     player = CONF.get('prefs', 'alert_player')
     # Apparently Zenity's timeout function is broken :(
-    #timeout = int(CONF.get('prefs', 'prompt_timeout'))
+    # timeout = int(CONF.get('prefs', 'prompt_timeout'))
     host = CONF.get('prefs', 'asterisk_host')
     port = int(CONF.get('prefs', 'host_port'))
     number = CONF.get('prefs', 'conf_number')
 
-    for delta in sorted(calls.keys()):
-        item, code = calls[delta]
-        msg = 'Call %s (Passcode %s). Call this?' % (item.get('summary'),
-                                                     code)
-        os.system('%s %s >/dev/null 2>&1 &' % (player, sound))
-        result = os.system("zenity --question --text='%s'" % (msg))
-        print result
-        if result == 0:
-            client.make_call(host, port, number, code)
-            break
+    msg = '%s (Passcode %s). Call this?' % (item.get('summary'),
+                                            code)
+    os.system('%s %s >/dev/null 2>&1 &' % (player, sound))
+    result = os.system("zenity --question --text='%s'" % (msg))
+    if result != 0:
+        return
+    if opts.dryrun:
+        print 'Would call %s %s %s %s' % (host, port, number, code)
+    else:
+        client.make_call(host, port, number, code)
+
+def tui_prompt_to_call_next(opts, delta, item, code):
+    host = CONF.get('prefs', 'asterisk_host')
+    port = int(CONF.get('prefs', 'host_port'))
+    number = CONF.get('prefs', 'conf_number')
+
+    print item.get('summary')
+    print '  Passcode %s' % code
+    print 'Call this? [y/N] ',
+    answer = sys.stdin.readline()
+    if answer.strip() != 'y':
+        return
+    if opts.dryrun:
+        print '\rWould call %s %s %s %s' % (host, port, number, code)
+    else:
+        client.make_call(host, port, number, code)
 
 def fetch_latest():
     return urllib.urlopen(CONF.get('prefs', 'gcal_url')).read()
@@ -78,7 +92,36 @@ def parse_config(conf_file='gcal_notifier.conf'):
     CONF = ConfigParser.ConfigParser()
     CONF.read(conf_file)
 
-if __name__ == '__main__':
+def parse_opts():
+    optparser = optparse.OptionParser()
+    optparser.add_option('-t', '--text',
+                         default=False, action='store_true',
+                         help='Text mode')
+    optparser.add_option('-m', '--minutes',
+                         default=None, type='int',
+                         help='Override lookahead_minutes')
+    optparser.add_option('-d', '--dryrun',
+                         default=False, action='store_true',
+                         help='Do not actually place a call')
+    opts, args = optparser.parse_args()
+    return opts
+
+def main():
+    opts = parse_opts()
     parse_config()
     caldata = fetch_latest()
-    maybe_call_next(caldata)
+    timerange = datetime.timedelta(minutes=opts.minutes or int(
+            CONF.get('prefs', 'lookahead_minutes')))
+    cal = icalendar.Calendar.from_ical(caldata)
+    calls = get_upcoming(cal, timerange)
+
+    for delta in sorted(calls.keys()):
+        item, code = calls[delta]
+
+        if opts.text:
+            tui_prompt_to_call_next(opts, delta, item, code)
+        else:
+            gui_prompt_to_call_next(opts, delta, item, code)
+
+if __name__ == '__main__':
+    main()
